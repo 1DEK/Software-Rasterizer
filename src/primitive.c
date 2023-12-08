2f9c8afd9b733d8debf4a7a0c98989c2767971ea
1DEK <abdullaidek@gmail.com>
2023-12-08 13:13:40 +0300

    triangle filling algorithm

--- src/primitive.c
+++ src/primitive.c
@@ -7,6 +7,12 @@ void drawLine(vec2 p1, vec2 p2, uint32_t color)
 	// more vertical line
 	if (fabs(dy) > fabs(dx))
 	{
+		if (dy < 0.0)
+		{
+			vec2 temp = p1;
+			p1 = p2;
+			p2 = temp;
+		}
 		float m = dx/dy;
 		float x = p1.x;
 		for (int y = p1.y; y <= p2.y; y++)
@@ -18,6 +24,12 @@ void drawLine(vec2 p1, vec2 p2, uint32_t color)
 	// more horizantal line
 	else
 	{
+		if (dx < 0.0)
+		{
+			vec2 temp = p1;
+			p1 = p2;
+			p2 = temp;
+		}
 		float m = dy/dx;
 		float y = p1.y;
 		for (int x = p1.x; x <= p2.x; x++)
@@ -37,11 +49,97 @@ void drawTriangleFrame(vec2 p1, vec2 p2, vec2 p3, uint32_t color)
 
 void drawTriangleFlat(vec2 p1, vec2 p2, vec2 p3, uint32_t color)
 {
-	// TODO
-	(void)p1;
-	(void)p2;
-	(void)p3;
-	(void)color;
+	// sort points by y-coordinate
+	if (p2.y < p1.y)
+	{
+		vec2 temp = p1;
+		p1 = p2;
+		p2 = temp;
+	}
+	if (p3.y < p2.y)
+	{
+		vec2 temp = p2;
+		p2 = p3;
+		p3 = temp;
+	}
+	if (p2.y < p1.y)
+	{
+		vec2 temp = p1;
+		p1 = p2;
+		p2 = temp;
+	}
+
+	// TODO these are floats, need to use a different way to compare probably, but IDK
+	// top left rule set up
+	if (p1.y == p2.y && p1.x < p2.x)
+	{
+		vec2 temp = p1;
+		p1 = p2;
+		p2 = temp;
+	}
+	else if (p2.y == p3.y && p2.x < p3.x)
+	{
+		vec2 temp = p2;
+		p2 = p3;
+		p3 = temp;
+	}
+
+	// p2 is on the left side
+	if (vec2Cross(vec2Subtract(p2, p1), vec2Subtract(p3, p1)).z < 0.0)
+	{
+		float x0 = p1.x;
+		float x1 = p1.x;
+		float m0 = (p2.x - p1.x)/(p2.y - p1.y);
+		float m1 = (p3.x - p1.x)/(p3.y - p1.y);
+		for (int y = p1.y; y < p2.y; y++)
+		{
+			for (int x = x0; x < x1; x++)
+			{
+				putPixel(x, y, color);
+			}
+			x0 += m0;
+			x1 += m1;
+		}
+		x0 = p2.x;
+		m0 = (p3.x - p2.x)/(p3.y - p2.y);
+		for (int y = p2.y; y <= p3.y; y++)
+		{
+			for (int x = x0; x < x1; x++)
+			{
+				putPixel(x, y, color);
+			}
+			x0 += m0;
+			x1 += m1;
+		}
+	}
+	// p2 is on the right side
+	else
+	{
+		float x0 = p1.x;
+		float x1 = p1.x;
+		float m0 = (p3.x - p1.x)/(p3.y - p1.y);
+		float m1 = (p2.x - p1.x)/(p2.y - p1.y);
+		for (int y = p1.y; y < p2.y; y++)
+		{
+			for (int x = x0; x < x1; x++)
+			{
+				putPixel(x, y, color);
+			}
+			x0 += m0;
+			x1 += m1;
+		}
+		x1 = p2.x;
+		m1 = (p3.x - p2.x)/(p3.y - p2.y);
+		for (int y = p2.y; y <= p3.y; y++)
+		{
+			for (int x = x0; x < x1; x++)
+			{
+				putPixel(x, y, color);
+			}
+			x0 += m0;
+			x1 += m1;
+		}		
+	}
 }
 
 void drawTriangleShaded(vec2 p1, uint32_t color1, vec2 p2, uint32_t color2, vec2 p3, uint32_t color3)
