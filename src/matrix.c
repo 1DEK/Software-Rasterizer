c9ae615530a6453f3953936ac124549c5c3ed801
1DEK <abdullaidek@gmail.com>
2023-12-10 16:05:59 +0300

    Cleaned up matrices and added functions to create translation and rotation matrices in 2D and 3D

--- src/matrix.c
+++ src/matrix.c
@@ -1,18 +1,55 @@
 #include "matrix.h"
 
-vec2 vec2Transform(const mat3 *transformation, vec2 v)
+vec2 mat2MultiplyVec2(const mat2 *m, vec2 v)
 {
 	return (vec2){
-		transformation->a11*v.x + transformation->a12*v.y + transformation->a13,
-		transformation->a21*v.x + transformation->a22*v.y + transformation->a23,
+		m->a11*v.x + m->a12*v.y,
+		m->a21*v.x + m->a22*v.y
 	};
 }
 
-vec3 vec3Transform(const mat4 *transformation, vec3 v)
+vec3 mat3MultiplyVec3(const mat3 *m, vec3 v)
 {
 	return (vec3){
-		transformation->a11*v.x + transformation->a12*v.y + transformation->a13*v.z + transformation->a14,
-		transformation->a21*v.x + transformation->a22*v.y + transformation->a23*v.z + transformation->a24,
-		transformation->a31*v.x + transformation->a32*v.y + transformation->a33*v.z + transformation->a34
+		m->a11*v.x + m->a12*v.y + m->a13*v.z,
+		m->a21*v.x + m->a22*v.y + m->a23*v.z,
+		m->a31*v.x + m->a32*v.y + m->a33*v.z
+	};
+}
+
+vec4 mat4MultiplyVec4(const mat4 *m, vec4 v)
+{
+	return (vec4){
+		m->a11*v.x + m->a12*v.y + m->a13*v.z + m->a14*v.w,
+		m->a21*v.x + m->a22*v.y + m->a23*v.z + m->a24*v.w,
+		m->a31*v.x + m->a32*v.y + m->a33*v.z + m->a34*v.w,
+		m->a41*v.x + m->a42*v.y + m->a43*v.z + m->a44*v.w
+	};
+}
+
+mat2 mat2MultiplyMat2(const mat2 *m1, const mat2 *m2)
+{
+	return (mat2){
+		m1->a11*m2->a11 + m1->a12*m2->a21, m1->a11*m2->a12 + m1->a12*m2->a22,
+		m1->a21*m2->a11 + m1->a22*m2->a21, m1->a21*m2->a12 + m1->a22*m2->a22,
+	};
+}
+
+mat3 mat3MultiplyMat3(const mat3 *m1, const mat3 *m2)
+{
+	return (mat3){
+		m1->a11*m2->a11 + m1->a12*m2->a21 + m1->a13*m2->a31, m1->a11*m2->a12 + m1->a12*m2->a22 + m1->a13*m2->a32, m1->a11*m2->a13 + m1->a12*m2->a23 + m1->a13*m2->a33,
+		m1->a21*m2->a11 + m1->a22*m2->a21 + m1->a23*m2->a31, m1->a21*m2->a12 + m1->a22*m2->a22 + m1->a23*m2->a32, m1->a21*m2->a13 + m1->a22*m2->a23 + m1->a23*m2->a33,
+		m1->a31*m2->a11 + m1->a32*m2->a21 + m1->a33*m2->a31, m1->a31*m2->a12 + m1->a32*m2->a22 + m1->a33*m2->a32, m1->a31*m2->a13 + m1->a32*m2->a23 + m1->a33*m2->a33
+	};
+}
+
+mat4 mat4MultiplyMat4(const mat4 *m1, const mat4 *m2)
+{
+	return (mat4){
+		m1->a11*m2->a11 + m1->a12*m2->a21 + m1->a13*m2->a31 + m1->a14*m2->a41, m1->a11*m2->a12 + m1->a12*m2->a22 + m1->a13*m2->a32 + m1->a14*m2->a42, m1->a11*m2->a13 + m1->a12*m2->a23 + m1->a13*m2->a33 + m1->a14*m2->a43, m1->a11*m2->a14 + m1->a12*m2->a24 + m1->a13*m2->a34 + m1->a14*m2->a44,
+		m1->a21*m2->a11 + m1->a22*m2->a21 + m1->a23*m2->a31 + m1->a24*m2->a41, m1->a21*m2->a12 + m1->a22*m2->a22 + m1->a23*m2->a32 + m1->a24*m2->a42, m1->a21*m2->a13 + m1->a22*m2->a23 + m1->a23*m2->a33 + m1->a24*m2->a43, m1->a21*m2->a14 + m1->a22*m2->a24 + m1->a23*m2->a34 + m1->a24*m2->a44,
+		m1->a31*m2->a11 + m1->a32*m2->a21 + m1->a33*m2->a31 + m1->a34*m2->a41, m1->a31*m2->a12 + m1->a32*m2->a22 + m1->a33*m2->a32 + m1->a34*m2->a42, m1->a31*m2->a13 + m1->a32*m2->a23 + m1->a33*m2->a33 + m1->a34*m2->a43, m1->a31*m2->a14 + m1->a32*m2->a24 + m1->a33*m2->a34 + m1->a34*m2->a44,
+		m1->a41*m2->a11 + m1->a42*m2->a21 + m1->a43*m2->a31 + m1->a44*m2->a41, m1->a41*m2->a12 + m1->a42*m2->a22 + m1->a43*m2->a32 + m1->a44*m2->a42, m1->a41*m2->a13 + m1->a42*m2->a23 + m1->a43*m2->a33 + m1->a44*m2->a43, m1->a41*m2->a14 + m1->a42*m2->a24 + m1->a43*m2->a34 + m1->a44*m2->a44
 	};
 }
--- src/matrix.h
+++ src/matrix.h
@@ -3,6 +3,12 @@
 
 #include "vector.h"
 
+typedef struct Mat2
+{
+	float a11, a12;
+	float a21, a22;
+} mat2;
+
 typedef struct Mat3
 {
 	float a11, a12, a13;
@@ -18,7 +24,17 @@ typedef struct Mat4
 	float a41, a42, a43, a44;
 } mat4;
 
-vec2 vec2Transform(const mat3 *transformation, vec2 v);
-vec3 vec3Transform(const mat4 *transformation, vec3 v);
+typedef struct Vec4
+{
+	float x, y, z, w;
+} vec4;
+
+vec2 mat2MultiplyVec2(const mat2 *m, vec2 v);
+vec3 mat3MultiplyVec3(const mat3 *m, vec3 v);
+vec4 mat4MultiplyVec4(const mat4 *m, vec4 v);
+
+mat2 mat2MultiplyMat2(const mat2 *m1, const mat2 *m2);
+mat3 mat3MultiplyMat3(const mat3 *m1, const mat3 *m2);
+mat4 mat4MultiplyMat4(const mat4 *m1, const mat4 *m2);
 
 #endif // MATRIX_H
